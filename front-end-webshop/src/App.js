9a2e5cabba4311b8d4900b15fc32445752b03763
l3ch3f <mfsoeropaiman@gmail.com>
2021-04-06 19:44:59 +0200

    ..

--- front-end-webshop/src/App.js
+++ front-end-webshop/src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import axios from "axios";
-import LoginPage from "./pages/login-page.component";
 
 function App() {
   axios.defaults.baseURL = "http://localhost:8081/api";
@@ -10,7 +9,7 @@ function App() {
       <Router>
         <Switch>
           <Route exact path="/">
-            <LoginPage />
+            
           </Route>
         </Switch>
       </Router>
