bf0d32dc02988ab0d1e19c354423d58e24895458
chen605 <chow_chen_1996@hotmail.com>
2021-04-06 21:42:03 +0200

    routing

--- front-end-webshop/src/App.js
+++ front-end-webshop/src/App.js
@@ -1,8 +1,19 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <div>
+          <Switch>
+            <Route path="/">{/* <Home /> */}</Route>
+          </Switch>
+        </div>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
