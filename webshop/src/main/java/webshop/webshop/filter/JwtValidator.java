a9415bde4b342e58216bf1aa45aa8c15c62fbd8d
l3ch3f <mfsoeropaiman@gmail.com>
2021-04-08 08:30:53 +0200

    ..

--- webshop/src/main/java/webshop/webshop/filter/JwtValidator.java
+++ webshop/src/main/java/webshop/webshop/filter/JwtValidator.java
@@ -14,6 +14,7 @@ import javax.crypto.SecretKey;
 import javax.servlet.http.HttpServletRequest;
 import java.nio.charset.StandardCharsets;
 
+// For confirming jwt when http request is done
 public class JwtValidator {
     public Authentication doFilterInternal(HttpServletRequest request) {
         String jwt = request.getHeader(SecurityConstants.JWT_HEADER);
